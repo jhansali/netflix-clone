dfd23a011164039bc1afd53d34e77082fc704039
jhansali <jhansali@asu.edu>
2024-11-17 02:00:03 -0700

    Add Google and Github support for login

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,6 +3,8 @@ import Input from '@/components/Input'
 import axios from 'axios';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
 
 const auth = () => {
 
@@ -84,6 +86,41 @@ const auth = () => {
                             <button onClick={variant === 'login' ? login:register} className='bg-red-600 py-3 text-white rounded-md w-full mt-10 transition'>
                                 {variant == 'login' ? 'Sign In' : 'Register'}
                             </button>
+                            <div className='flex flex-row items-center gap-4 mt-8 justify-center'>
+                                <div
+                                    onClick={() => signIn('google', {callbackUrl: '/'})}
+                                    className='
+                                        w-10
+                                        h-10
+                                        bg-white
+                                        rounded-full
+                                        flex
+                                        items-center
+                                        justify-center
+                                        cursor-pointer
+                                        hover:opacity-80
+                                        transition
+                                    '>
+                                        <FcGoogle size={30}/>
+                                </div>
+                                <div
+                                    onClick={() => signIn('github', {callbackUrl: '/'})}
+                                    className='
+                                        w-10
+                                        h-10
+                                        bg-white
+                                        rounded-full
+                                        flex
+                                        items-center
+                                        justify-center
+                                        cursor-pointer
+                                        hover:opacity-80
+                                        transition
+                                    '>
+                                        <FaGithub size={30}/>
+                                </div>
+
+                            </div>
                             <p className='text-neutral-500 mt-12'>
                                 {variant == 'login' ? 'New to Codyflix?' : 'Already have an account?'}
                                 <span onClick={toggleVariant} className='text-white ml-1 hover:underline cursor-pointer'>
